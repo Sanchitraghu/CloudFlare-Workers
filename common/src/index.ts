696248804d81ec6d24f89b695f682b3cb3f6203d
sanchit raghuwanshi <sanchit.raghu113@gmail.com>
2025-05-08 11:33:50 +0530

    Added createdAt and updatedAt in Post schema and added blog validation in common

--- common/src/index.ts
+++ common/src/index.ts
@@ -11,6 +11,15 @@ export const signInValidation = z.object({
     password: z.string().min(6),
 });
 
+export const blogContentValidation = z.object({
+    title: z.string().min(3),
+    content: z.string().min(10),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+})
+
 export type SignUpType = z.infer<typeof signUpValidation>;
 
 export type SignInType = z.infer<typeof signInValidation>;
+
+export type BlogContentType = z.infer<typeof blogContentValidation>;
